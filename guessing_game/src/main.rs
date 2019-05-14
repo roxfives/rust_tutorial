c844d2567e1eb0b14536893545a6d015bf81b187
Ricardo Chagas <ricardo.chagas2@outlook.com>
2019-05-14 16:49:43 -0300

    Compares typed value to the secret one

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,12 +1,26 @@
 use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
+    let secret_number = rand::thread_rng().gen_range(1, 101);
     let mut guess = String::new();
 
     println!("Type your guess!");
     
     io::stdin().read_line(&mut guess)
         .expect("Failed to read input line");
+    let guess: u32 = guess.trim().parse()
+        .expect("Please, type a number"); 
 
+    match guess.cmp(&secret_number) {
+        Ordering::Less => println!("Too low!"),
+        Ordering::Equal => println!("You win!"),
+        Ordering::Greater => println!("Too big!"),
+    }
+
+    println!("The secret number: {}", secret_number);
     println!("You guessed {}", guess);
+
+    
 }
