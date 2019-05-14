5c706c9eee87221443dc2201348f2172575f4d01
Ricardo Chagas <ricardo.chagas2@outlook.com>
2019-05-14 16:55:52 -0300

    Quits when the user input the correct guess

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -4,23 +4,27 @@ use rand::Rng;
 
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
-    let mut guess = String::new();
-
     println!("Type your guess!");
     
-    io::stdin().read_line(&mut guess)
-        .expect("Failed to read input line");
-    let guess: u32 = guess.trim().parse()
-        .expect("Please, type a number"); 
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too low!"),
-        Ordering::Equal => println!("You win!"),
-        Ordering::Greater => println!("Too big!"),
-    }
-
     println!("The secret number: {}", secret_number);
-    println!("You guessed {}", guess);
 
-    
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess)
+            .expect("Failed to read input line");
+        
+        let guess: u32 = guess.trim().parse()
+            .expect("Please, type a number"); 
+
+        println!("You guessed {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too low!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }    
 }
