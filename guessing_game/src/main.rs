7b57f37198a7c5d723fad9850b0127127813eb3c
Ricardo Chagas <ricardo.chagas2@outlook.com>
2019-05-14 17:15:50 -0300

    Puts output messagens on constant strings

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -2,32 +2,40 @@ use std::io;
 use std::cmp::Ordering;
 use rand::Rng;
 
+const INITIAL_MESSAGE: &str = "##################### Type your guess! #####################";
+const FAILED_STDIN_MESSAGE: &str = "Failed to read input line";
+const INVALID_INPUT_MESSAGE: &str = "Please, print a valid integer!";
+const GUESS_DESCRIPTION: &str = "You guessed ";
+const TOO_LOW_MESSAGE: &str = "Too low!";
+const TOO_BIG_MESSAGE: &str = "Too big!";
+const WINNING_MESSAGE: &str = "##################### You win! #####################";
+
 fn main() {
     let secret_number = rand::thread_rng().gen_range(1, 101);
     
-    println!("##################### Type your guess! #####################");
+    println!("{}", INITIAL_MESSAGE);
 
     loop {
         let mut guess = String::new();
 
         io::stdin().read_line(&mut guess)
-            .expect("Failed to read input line");
+            .expect(FAILED_STDIN_MESSAGE);
         
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
             Err(_) => {
-                println!("Please, print a valid integer!");
+                println!("{}", INVALID_INPUT_MESSAGE);
                 continue;
             },
         }; 
 
-        println!("You guessed {}", guess);
+        println!("{} {}", GUESS_DESCRIPTION, guess);
 
         match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too low!"),
-            Ordering::Greater => println!("Too big!"),
+            Ordering::Less => println!("{}", TOO_LOW_MESSAGE),
+            Ordering::Greater => println!("{}", TOO_BIG_MESSAGE),
             Ordering::Equal => {
-                println!("##################### You win! #####################");
+                println!("{}", WINNING_MESSAGE);
                 break;
             }
         }
